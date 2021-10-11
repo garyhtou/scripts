d89960d0dca1a34f878a783e732efb3c708e5bde
Gary Tou <gary@garytou.com>
2021-10-10 23:42:23 -0700

    add more comments

--- notes.sh
+++ notes.sh
@@ -6,12 +6,12 @@
 # alias notes=". /path/to/this/file/notes.sh"
 
 
-
 # Save current working directory so that we can return the user back
 # to where they started
 pwd=`pwd`
 
-# Change directory to the location of the notes
+# Change directory to the location of the notes.
+# Update the following line to reflect the path of your notes.
 cd ~/Google\ Drive/My\ Drive/Files/Notes
 
 # If a file name was passed in, open it in vim (assumed to be .txt)
@@ -19,7 +19,7 @@ if [ ! -z "$1" ]
   then
     vim "$1.txt"
 
-    # Since the user specific a specific note to open, we assume that
+    # Since the user specified a note to open, we assume that
     # they will not be accessing any other notes and would like to be
     # returned to their original working directory
     cd $pwd
